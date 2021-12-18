2ad081ce251b64fc3a10922a77db97ee010ea9a5
varshitha1707 <varshithasreddy@gmail.com>
2021-12-18 13:14:38 +0530

    1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div >
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Landing/>} />
           <Route path="/Join-us" element={<Doc/>} />
